01b5a08a0719442d7fece5e219f0cf041c478a7d
Telvin <telvinzhong@gmail.com>
2021-02-22 21:26:47 -0800

    changed buttons on grid

--- src/components/course-grid.js
+++ src/components/course-grid.js
@@ -3,11 +3,16 @@ import CourseCard from "./course-card";
 import {Link} from "react-router-dom";
 
 const CourseGrid = ({courses}) =>
-  <div>
+    <div className = "container-fluid">
       <Link to="/courses/table">
         <i className="fas fa-list fa-2x float-right"></i>
       </Link>
+
+      <i className="fas fa-2x fa-sort float-right"> </i>
+      <i className="fas fa-2x fa-folder float-right"> </i>
+
     <h2>Course Grid {courses.length}</h2>
+
     <div className="row">
     {
       courses.map(course =>
