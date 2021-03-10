5523d66850533d33994d3503f71f23b7b3ecb32d
Telvin <telvinzhong@gmail.com>
2021-03-09 23:27:19 -0800

    modified services

---
+++ src/services/topic-service.js
@@ -0,0 +1,39 @@
+const LESSONS_URL = "https://wbdv-generic-server.herokuapp.com/api/001046097/lessons"
+const TOPICS_URL = "https://wbdv-generic-server.herokuapp.com/api/001046097/topics"
+
+export const createTopic = (lessonId, topic) =>
+    fetch(`${LESSONS_URL}/${lessonId}/topics`, {
+        method: "POST",
+        headers: {
+            'content-type': 'application/json'
+        },
+        body: JSON.stringify(topic)
+    })
+        .then(response => response.json())
+
+export const findTopicsForLesson = (lessonId) =>
+    fetch(`${LESSONS_URL}/${lessonId}/topics`)
+        .then(response => response.json())
+
+export const updateTopic = (topicId, topic) =>
+    fetch(`${TOPICS_URL}/${topicId}`, {
+        method: "PUT",
+        headers: {
+            'content-type': 'application/json'
+        },
+        body: JSON.stringify(topic)
+    })
+        .then(response => response.json())
+
+export const deleteTopic = (topicId) =>
+    fetch(`${TOPICS_URL}/${topicId}`, {
+        method: "DELETE"
+    })
+        .then(response => response.json())
+
+export default {
+    createTopic,
+    findTopicsForLesson,
+    updateTopic,
+    deleteTopic
+}
