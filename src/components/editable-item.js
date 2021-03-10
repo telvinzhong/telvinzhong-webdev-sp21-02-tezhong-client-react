7eaab6222162469e1ea328cef93ba630d8f94abd
Telvin <telvinzhong@gmail.com>
2021-03-09 23:27:38 -0800

    created editable items

---
+++ src/components/editable-item.js
@@ -0,0 +1,54 @@
+import React, {useState} from 'react';
+import {Link} from "react-router-dom";
+
+const EditableItem = (
+    {
+        to="/somewhere/to/go",
+        item= {title: "Some Title", _id:"ABC"},
+        deleteItem,
+        updateItem,
+        active
+    }) => {
+    const [editing, setEditing] = useState(false)
+    const [cachedItem, setCachedItem] = useState(item)
+
+    return (
+        <>
+            {
+                !editing &&
+                <>
+                    <Link className={`nav-link ${active? 'active' : ''}`} to={to}>
+                        {item.title}
+                    </Link>
+
+                    <i onClick={() => setEditing(true)} className="fas fa-edit btn btn-sm"></i>
+                </>
+            }
+            {
+                editing &&
+                <>
+                    <input
+                        onChange={(event) =>
+                            setCachedItem({
+                                ...cachedItem,
+                                title: event.target.value
+                            })}
+                        value={cachedItem.title}
+                    />
+
+                    <i onClick={() => {
+                        setEditing(false)
+                        updateItem(cachedItem)
+                    }} className="fas fa-check btn btn-sm"></i>
+                    <i onClick={() => {
+                        setEditing(false)
+                        deleteItem(item)
+                    }} className="fas fa-times btn btn-sm"></i>
+                </>
+            }
+
+        </>
+    )
+}
+
+export default EditableItem;
