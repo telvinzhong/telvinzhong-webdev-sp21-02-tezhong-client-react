24fce8be4022cd2c1ef34aca3f3774b2a875bbfe
Telvin <telvinzhong@gmail.com>
2021-03-21 18:00:25 -0700

    modified services

--- src/services/lesson-service.js
+++ src/services/lesson-service.js
@@ -5,38 +5,32 @@ export const findLessonsForModule = (moduleId) =>
     fetch(`${MODULES_URL}/${moduleId}/lessons`)
         .then(response => response.json())
 
-export const findLesson = (lessonId) => {
-}
-
 export const createLessonForModule = (moduleId, lesson) =>
-    fetch(`${MODULES_URL}/${moduleId}/lessons`, {
-        method: 'POST',
-        headers: {
-            'content-type': 'application/json'
-        },
-        body: JSON.stringify(lesson)
-    })
-        .then(response => response.json())
-
-export const updateLesson = (lessonId, lesson) =>
-    fetch(`${LESSONS_URL}/${lessonId}`, {
-        method: "PUT",
+    fetch(`${MODULES_URL}/${moduleId}/lessons`,{
+        method: "POST",
         body: JSON.stringify(lesson),
-        headers: {
+        headers:{
             'content-type': 'application/json'
-        },
-    })
-        .then(response => response.json())
+        }
+    }).then(response => response.json())
 
 export const deleteLesson = (lessonId) =>
-    fetch(`${LESSONS_URL}/${lessonId}`, {
+    fetch(`${LESSON_URL}/${lessonId}`,{
         method: "DELETE"
-    })
-        .then(response => response.json())
+    }).then(response => response.json())
+
+export const updateLesson = (lessonId, lesson) =>
+    fetch(`${LESSON_URL}/${lessonId}`,{
+        method: "PUT",
+        body: JSON.stringify(lesson),
+        headers:{
+            'content-type': 'application/json'
+        }
+    }).then(response => response.json())
 
 export default {
     findLessonsForModule,
     createLessonForModule,
-    updateLesson,
-    deleteLesson
+    deleteLesson,
+    updateLesson
 }
---
+++ src/services/widget-service.js
@@ -0,0 +1,45 @@
+const API_URL = "https://tezhong-5610-two.herokuapp.com/api";
+
+export const findAllWidgets = () =>
+    fetch(`${API_URL}/widgets`)
+        .then(response => response.json())
+
+
+export const findWidgetsForTopic = (topicId) =>
+    fetch(`${API_URL}/topics/${topicId}/widgets`)
+        .then(response => response.json())
+
+
+export const createWidget = (topicId, widget) =>
+    fetch(`${API_URL}/topics/${topicId}/widgets`, {
+        method: "POST",
+        body: JSON.stringify(widget),
+        headers: {
+            'content-type': 'application/json'
+        }
+    }).then(response => response.json())
+
+
+export const updateWidget = (widgetId, widget) =>
+    fetch(`${API_URL}/widgets/${widgetId}`, {
+        method: "PUT",
+        body: JSON.stringify(widget),
+        headers: {
+            'content-type': 'application/json'
+        }
+    }).then(response => response.json())
+
+
+export const deleteWidget = (widgetId) =>
+    fetch(`${API_URL}/widgets/${widgetId}`, {
+        method: "DELETE"
+    }).then(response => response.json())
+
+
+export default {
+    findAllWidgets,
+    findWidgetsForTopic,
+    createWidget,
+    updateWidget,
+    deleteWidget
+}
