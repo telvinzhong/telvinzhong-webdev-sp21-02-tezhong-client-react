f245e0d4ab046e3d78533d1ef6935ab3dbbe973d
Telvin <telvinzhong@gmail.com>
2021-04-19 22:26:56 -0700

    quiz service

---
+++ src/services/quiz-service.js
@@ -0,0 +1,31 @@
+const QUIZ_URL = "http://localhost:4000/api/quizzes"
+
+export const findQuizzesForCourse = () =>
+    fetch(QUIZ_URL)
+        .then(response => response.json())
+
+
+export const findQuestionForQuiz = (quizId) =>
+    fetch(`${QUIZ_URL}/${quizId}/questions`)
+        .then(response => response.json())
+
+export const submitQuiz = (quizId, questions) => {
+    fetch(`${QUIZ_URL}/${quizId}/attempts`, {
+        method: 'POST',
+        body: JSON.stringify(questions),
+        headers: {
+            'content-type': 'application/json'
+        }
+    }).then(response => response.json())
+}
+
+export const findAttemptsForQuiz = (quizId) =>
+    fetch(`${QUIZ_URL}/${quizId}/attempts`)
+        .then(response => response.json())
+
+export default {
+    findQuizzesForCourse,
+    findQuestionForQuiz,
+    submitQuiz,
+    findAttemptsForQuiz
+}
